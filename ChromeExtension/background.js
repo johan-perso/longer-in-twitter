e23b01d2e341c9687352775d0d278304b711d6c2
Johan le stickman <Johan_Stickman@proton.me>
2022-07-19 23:18:04 +0200

    Update background.js

--- ChromeExtension/background.js
+++ ChromeExtension/background.js
@@ -3,13 +3,21 @@ console.log("[Longer-In-Twitter] background.js loaded");
 
 // Quand un message (demande pour obtenir un TwitLonger) est reçu
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-    getTwitLonger(msg.text).then(content => sendResponse(content));
+    if(msg.type == 'twitlonger') getTwitLonger(msg.text).then(content => sendResponse(content));
+    if(msg.type == 'xltweet') getXlTweet(msg.text).then(content => sendResponse(content));
     return true;
 });
 
 // Fonction pour obtenir le contenu d'un TwitLonger
 async function getTwitLonger(id){
     console.log(`[Longer-In-Twitter] requesting content of TwitLonger by id "${id}"`);
-    var content = await fetch(`https://twitlonger-api-unofficial.johanstickman.com/getContent/${id}`).then(res => res.text())
+    var content = await fetch(`https://twitlonger-api-unofficial.johanstickman.com/twitlonger/${id}`).then(res => res.text())
+    return content;
+}
+
+// Fonction pour obtenir le contenu d'un XLTweet
+async function getXlTweet(id){
+    console.log(`[Longer-In-Twitter] requesting content of XLTweet by id "${id}"`);
+    var content = await fetch(`https://twitlonger-api-unofficial.johanstickman.com/xltweet/${id}`).then(res => res.text())
     return content;
 }

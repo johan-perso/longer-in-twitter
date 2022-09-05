d958b1db7dedad4cce57043df71e644af69521a5
Johan le stickman <Johan_Stickman@proton.me>
2022-09-05 21:06:51 +0200

    Update background.js

--- ChromeExtension/background.js
+++ ChromeExtension/background.js
@@ -11,13 +11,13 @@ chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
 // Fonction pour obtenir le contenu d'un TwitLonger
 async function getTwitLonger(id){
     console.log(`[Longer-In-Twitter] requesting content of TwitLonger by id "${id}"`);
-    var content = await fetch(`https://twitlonger-api-unofficial.johanstickman.com/twitlonger/${id}`).then(res => res.text())
+    var content = await fetch(`https://twitlonger-api-unofficial.johanstick.me/twitlonger/${id}`).then(res => res.text())
     return content;
 }
 
 // Fonction pour obtenir le contenu d'un XLTweet
 async function getXlTweet(id){
     console.log(`[Longer-In-Twitter] requesting content of XLTweet by id "${id}"`);
-    var content = await fetch(`https://twitlonger-api-unofficial.johanstickman.com/xltweet/${id}`).then(res => res.text())
+    var content = await fetch(`https://twitlonger-api-unofficial.johanstick.me/xltweet/${id}`).then(res => res.text())
     return content;
 }
